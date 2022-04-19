2db3ea2f30103828678fc1089a5d6a474f5a44a2
aileendugan <adugan2@nd.edu>
2022-04-19 01:06:19 -0400

    trial1textures+projMatrix

--- fWiz.js
+++ fWiz.js
@@ -3,9 +3,11 @@
 var gl;
 var p;
 var vertices;
-var numOfFans;
+var numOfFans = 36;
 var colors;
 var u_ColorLoc;
+var program;
+
 
 // Move base variables
 //var go = 0;
@@ -39,8 +41,52 @@ var xVelocity, yVelocity;
 var xCenter, yCenter;
 var xFrogCenter, yFrogCenter;
 var u_vCenterLoc;
+var a_vPositionLoc;
+var vBuffer;
+
+var a_TextureCoordLoc;
+var u_TextureSamplerLoc;
+var textureCoordData = [0, 1, 0, 0, 1, 0, 1, 1];
+var textures = [];
+//var BadMuffinTexture;
+var TextureCoordBuffer;
+//var BadMuffinVertexTextureCoordBuffer;
+var ctmLoc;
+var ctm;
+
 
 var GameOver = false;
+
+
+function handleLoadedTexture(texture) {
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.uniform1i(u_TextureSamplerLoc, 0);
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+    gl.pixelStorei(gl.UNPACK_PREMULTIPLY_ALPHA_WEBGL, true);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, texture.image);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    //gl.bindTexture(gl.TEXTURE_2D, texture);
+    //gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, texture.image);
+    //gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    //gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
+    // prevent s wrap
+    //gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    // Prevents t-coordinate wrapping (repeating).
+    //gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    //gl.generateMipmap(gl.TEXTURE_2D);
+}
+
+function initTexture(textID) {
+    var texture = gl.createTexture();
+    texture.image = new Image();
+    texture.image.src = textID;
+    textures.push(texture);
+
+}
 
 window.onload = function init() {
     var canvas = document.getElementById("gl-canvas");
@@ -63,7 +109,7 @@ window.onload = function init() {
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
     //  Load shaders and initialize attribute buffers
-    var program = initShaders(gl, "vertex-shader", "fragment-shader");
+    program = initShaders(gl, "vertex-shader", "fragment-shader");
     gl.useProgram(program);
 
     // Load the data into the GPU
@@ -71,12 +117,38 @@ window.onload = function init() {
     gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
 
+    a_TextureCoordLoc = gl.getAttribLocation(program, "a_textCoord");
+
+    u_TextureSamplerLoc = gl.getUniformLocation(program, "u_TextureSampler");
+
     // Associate out shader variables with our data buffer
     var a_vPositionLoc = gl.getAttribLocation(program, "a_vPosition");
-    gl.vertexAttribPointer(a_vPositionLoc, 2, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(a_vPositionLoc);
+    //gl.vertexAttribPointer(a_vPositionLoc, 2, gl.FLOAT, false, 0, 0);
+    //gl.enableVertexAttribArray(a_vPositionLoc);
+
+    // texture coord buffer Goodmuffin
+    TextureCoordBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, TextureCoordBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoordData), gl.STATIC_DRAW);
+    gl.enableVertexAttribArray(a_TextureCoordLoc);
+    gl.vertexAttribPointer(a_TextureCoordLoc, 2, gl.FLOAT, false, 0, 0);
+    // texture coord buffer BadMuffin
+    //BadMuffinVertexTextureCoordBuffer = gl.createBuffer();
+    //gl.bindBuffer(gl.ARRAY_BUFFER, BadMuffinVertexTextureCoordBuffer);
+    //gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoordData), gl.STATIC_DRAW);
+    //texture coords
+    //a_TextureCoordLoc = gl.getAttribLocation(program, "a_TextureCoord");
+    //gl.enableVertexAttribArray(a_TextureCoordLoc);
+
+    //gl.activeTexture(gl.TEXTURE0);
+    //u_TextureSamplerLoc = gl.getUniformLocation(program, "u_TextureSampler");
+    //gl.uniform1i(u_TextureSamplerLoc, 0);
 
     u_ColorLoc = gl.getUniformLocation(program, "u_Color");
+    ctmLoc = gl.getUniformLocation(program, "ctMatrix");
+    var pmLoc = gl.getUniformLocation(program, "projMatrix");
+    var pm = ortho(-1.0, 1.0, -0.5, 0.5, -1.0, 1.0);
+    gl.uniformMatrix4fv(pmLoc, false, flatten(pm));
 
     // associate base/bullet center with uniform shader variable
     u_xMoveLoc = gl.getUniformLocation(program, "u_xMove");
@@ -84,6 +156,8 @@ window.onload = function init() {
     // associate invader center with uniform shader variable
     u_vCenterLoc = gl.getUniformLocation(program, "u_vCenter");
 
+    initTexture("./muffinGood.jpg");
+    initTexture("./muffinBad.jpg");
     moveFrog();
 
     document.getElementById("Start").onclick = function() {
@@ -115,6 +189,7 @@ function setup() {
     yFrogCenter = 0.925;
     ///////////////// Good Muffin //////////////////////
     vertices.push(vec2(0.06, 0.94));
+    textureCoordData.push(vec2(0.06, 0.94));
     // set up radius 
     var rgood = 0.06;
     //var numOfFansg = 36;
@@ -125,11 +200,20 @@ function setup() {
             vertices[38][0] + rgood * Math.cos(j * Math.PI / numOfFans),
             vertices[38][1] + rgood * Math.sin(j * Math.PI / numOfFans)
         ));
+        textureCoordData.push(vec2(
+            vertices[38][0] + rgood * Math.cos(j * Math.PI / numOfFans),
+            vertices[38][1] + rgood * Math.sin(j * Math.PI / numOfFans)
+        ));
+
     }
     vertices.push(vec2(0.0, 0.94));
+    textureCoordData.push(vec2(0.0, 0.94));
     vertices.push(vec2(0.035, 0.88));
+    textureCoordData.push(vec2(0.035, 0.88));
     vertices.push(vec2(0.085, 0.88));
+    textureCoordData.push(vec2(0.085, 0.88));
     vertices.push(vec2(0.12, 0.94));
+    textureCoordData.push(vec2(0.12, 0.94));
     ///////////////// Bad Muffin //////////////////////
     vertices.push(vec2(-0.06, 0.94));
     // set up radius 
@@ -264,26 +348,105 @@ function drawLives() {
     lives.innerHTML = string;
 }
 
+function drawFrog() {
+    var tm, sm, rm, scaling_l;
+    //draw frog
+    handleLoadedTexture(textures[0]);
+    gl.enableVertexAttribArray(a_vPositionLoc);
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.vertexAttribPointer(a_vPositionLoc, 2, gl.FLOAT, false, 0, 0);
+
+    scaling_l = 0.05;
+    rm = rotateZ(0);
+    sm = scalem(scaling_l, scaling_l, scaling_l);
+    tm = translate(xFrogCenter, yFrogCenter, 0.0); //might need to change
+
+    ctm = mat4();
+    ctm = mult(rm, ctm);
+    ctm = mult(sm, ctm);
+    ctm = mult(tm, ctm);
+    gl.uniform3fv(u_ColorLoc, colors[0]);
+    gl.uniformMatrix4fv(ctmLoc, false, flatten(ctm));
+    gl.drawArrays(gl.TRIANGLE_FAN, 0, numOfFans + 2);
+}
+
+function drawGmuffin() {
+    var tm, sm, rm, scale_x, scale_y, scale_z;
+    gl.enableVertexAttribArray(a_vPositionLoc);
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.vertexAttribPointer(a_vPositionLoc, 2, gl.FLOAT, false, 0, 0);
+    handleLoadedTexture(textures[0]);
+
+    scale_x = 0.15;
+    scale_y = 0.075;
+    scale_z = 0.1;
+    sm = scalem(scale_x, scale_y, scale_z);
+    tm = translate(xGood, yGood, 0.0);
+    ctm = mat4();
+    ctm = mult(sm, ctm);
+    ctm = mult(tm, ctm);
+    gl.uniform3fv(u_ColorLoc, colors[1]);
+    gl.uniformMatrix4fv(ctmLoc, false, flatten(ctm));
+    gl.drawArrays(gl.TRIANGLE_FAN, 38, 42);
+
+}
+
+function drawBmuffin() {
+    var tm, sm, rm, scale_x, scale_y, scale_z;
+    gl.enableVertexAttribArray(a_vPositionLoc);
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.vertexAttribPointer(a_vPositionLoc, 2, gl.FLOAT, false, 0, 0);
+    handleLoadedTexture(textures[1]);
+
+    scale_x = 0.15;
+    scale_y = 0.075;
+    scale_z = 0.1;
+    sm = scalem(scale_x, scale_y, scale_z);
+    tm = translate(xBad, yBad, 0.0);
+    ctm = mat4();
+    ctm = mult(sm, ctm);
+    ctm = mult(tm, ctm);
+    gl.uniform3fv(u_ColorLoc, colors[2]);
+    gl.uniformMatrix4fv(ctmLoc, false, flatten(ctm));
+    gl.drawArrays(gl.TRIANGLE_FAN, 80, 42);
+
+}
+
 function render() {
     gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.clearColor(0.8, 0.8, 0.8, 1.0);
+    gl.clearColor(0.5, 0.6, 1.0, 1.0);
 
     animate();
     // Frog //
     xFrogCenter = (xFrogMove * 0.1);
-    gl.uniform2fv(u_vCenterLoc, vec2(xFrogCenter, yFrogCenter));
-    gl.uniform3fv(u_ColorLoc, colors[0]);
-    gl.drawArrays(gl.TRIANGLE_FAN, 0, numOfFans + 2);
+    drawFrog();
+    //gl.uniform2fv(u_vCenterLoc, vec2(xFrogCenter, yFrogCenter));
+    //gl.uniform3fv(u_ColorLoc, colors[0]);
+    //gl.drawArrays(gl.TRIANGLE_FAN, 0, numOfFans + 2);
 
     // Good Muffin //
-    gl.uniform2fv(u_vCenterLoc, vec2(xGood, yGood));
-    gl.uniform3fv(u_ColorLoc, colors[1]);
-    gl.drawArrays(gl.TRIANGLE_FAN, 38, 42);
+    drawGmuffin();
+    //gl.bindTexture(gl.TEXTURE_2D, GoodMuffinTexture);
+    //a_TextureCoordLoc = gl.getAttribLocation(program, "a_TextureCoord");
+    //gl.enableVertexAttribArray(a_TextureCoordLoc);
+    //gl.activeTexture(gl.TEXTURE0);
+
+    //gl.bindBuffer(gl.ARRAY_BUFFER, GoodMuffinVertexTextureCoordBuffer);
+    //gl.vertexAttribPointer(a_TextureCoordLoc, 2, gl.FLOAT, false, 0, 0);
+    //u_TextureSamplerLoc = gl.getUniformLocation(program, "u_TextureSampler");
+    //gl.uniform1i(u_TextureSamplerLoc, 0);
+
+    //gl.uniform2fv(u_vCenterLoc, vec2(xGood, yGood));
+    //gl.uniform3fv(u_ColorLoc, colors[1]);
+    //gl.drawArrays(gl.TRIANGLE_FAN, 38, 42);
 
     // Bad Muffin //
-    gl.uniform2fv(u_vCenterLoc, vec2(xBad, yBad));
-    gl.uniform3fv(u_ColorLoc, colors[2]);
-    gl.drawArrays(gl.TRIANGLE_FAN, 80, 42);
+    drawBmuffin();
+    //gl.bindBuffer(gl.ARRAY_BUFFER, BadMuffinVertexTextureCoordBuffer);
+    //gl.vertexAttribPointer(a_TextureCoordLoc, 2, gl.FLOAT, false, 0, 0);
+    //gl.uniform2fv(u_vCenterLoc, vec2(xBad, yBad));
+    //gl.uniform3fv(u_ColorLoc, colors[2]);
+    //gl.drawArrays(gl.TRIANGLE_FAN, 80, 42);
 
     // Grass //
     gl.uniform2fv(u_vCenterLoc, vec2(0.0, 0.925));
